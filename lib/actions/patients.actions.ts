a9902c0d719d52c36490373cb3147108459b59b0
nemesisvoid <samsonajibade40@gmail.com>
2024-08-03 11:27:15 +0100

    minor changes

--- lib/actions/patients.actions.ts
+++ lib/actions/patients.actions.ts
@@ -7,17 +7,20 @@ import { InputFile } from 'node-appwrite/file';
 
 export const createUser = async (user: CreateUserParams) => {
   try {
+    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
     const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name);
-    console.log(newUser);
+
     return parseStringify(newUser);
   } catch (error: any) {
+    // Check existing user
     if (error && error?.code === 409) {
-      const documents = await users.list([Query.equal('email', [user.email])]);
-      return documents?.users[0];
+      const existingUser = await users.list([Query.equal('email', [user.email])]);
+
+      return existingUser.users[0];
     }
+    console.error('An error occurred while creating a new user:', error);
   }
 };
-
 export const getUser = async (userId: string) => {
   try {
     const user = await users.get(userId);
